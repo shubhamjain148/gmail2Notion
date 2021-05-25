b6449a543c42b8dd3764448293ef69577f189232
Shubham Jain <shubham.jain148@gmail.com>
2021-05-25 22:46:02 +0530

    database url change for alchemy

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     NOTION_SECRET_KEY = os.getenv("NOTION_SECRET_KEY", "this-is-the-default-key")
     GMAIL_CLIENT_ID = os.getenv("GMAIL_CLIENT_ID", "random")
     GMAIL_CLIENT_SECRET = os.getenv("GMAIL_CLIENT_SECRET", "random")
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 class ProductionConfig(Config):
     pass
